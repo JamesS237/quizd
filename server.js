ffce46a8ce9796cdbedb893a60bcb080aac4d0ad
Arvoreniad <alexnielsen12@gmail.com>
2014-03-22 18:22:53 +1100

    server.js passing jshint

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+	res.send('hello world');
 });
 
 app.listen(3000);
