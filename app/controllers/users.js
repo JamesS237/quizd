2518f53629a1ad226bfed77b76c7f933de319dbc
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 15:16:28 +1100

    added some passport middlware

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,21 +1,6 @@
 'use strict';
 
 exports.authenticate = function(req, res) {
-    res.render('index');
-	  if (!(req.body.username === 'john.doe' && req.body.password === 'foobar')) {
-	    res.send(401, 'Wrong user or password');
-	    return;
-	  }
-
-	  var profile = {
-	    first_name: 'John',
-	    last_name: 'Doe',
-	    email: 'john@doe.com',
-	    id: 123
-	  };
-
-	  // We are sending the profile inside the token
-	  var token = jwt.sign(profile, secret, { expiresInMinutes: 60*5 });
-
-	  res.json({ token: token });
+	var token = jwt.sign(profile, secret, { expiresInMinutes: 60*5 });
+	res.json({ token: token });
 };
--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,16 @@
 module.exports = function(app, passport) {
 	var staticPages = require('./controllers/static');
 	var users = require('./controllers/users');
-	app.get('/:anything', staticPages.home);
-	app.get('/', staticPages.home);
+
 
 	var expressJwt = require('express-jwt');
 	var secret = 'this is a temporary secret for testing purposes only';
 	app.use('/api', expressJwt({secret: secret}));
+	app.post('/authentciate', passport.authenticate('local', {
+        failureRedirect: '/signin',
+        failureFlash: true
+    }), users.authenticate);
 
+	app.get('/:anything', staticPages.home);
+	app.get('/', staticPages.home);
 };
