2c13d71d3336faa8bb2b8cd78f62c950781bb766
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 14:54:12 +1100

    added middleware config for token auth

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
 	var staticPages = require('./controllers/static');
 	app.get('/:anything', staticPages.home);
 	app.get('/', staticPages.home);
+
+	var expressJwt = require('express-jwt');
+	var secret = 'this is a temporary secret for testing purposes only';
+	app.use('/api', expressJwt({secret: secret}));
 };
