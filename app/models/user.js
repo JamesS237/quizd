ed57bb27a71993e0241d855fec2d679a22ed4500
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 15:50:26 +1100

    removed debug log

--- app/models/user.js
+++ app/models/user.js
@@ -22,17 +22,11 @@ var UserSchema = new Schema({
 
 UserSchema.pre('save', function(next) {
     var user = this;
-    console.log('24');
     if (!user.isModified('password')) return next();
-    console.log('26');
     bcrypt.genSalt(10, function(err, salt) {
         if (err) return next(err);
-            console.log('29');
-
         bcrypt.hash(user.password, salt, function(err, hash) {
-            console.log('32');
             if (err) return next(err);
-                console.log('33');
 
             user.password = hash;
             next();
