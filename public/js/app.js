badb42612b587d8bb7d54a93a129deb713b52fb1
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 13:43:26 +1100

    basic templating working

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-var quizd = angular.module('arcana', ['ngRoute', 'ngResource']);
+'use strict';
+
+var quizd = angular.module('quizd', ['ngRoute', 'ngResource']);
+
+quizd.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider.when('/', {
+		templateUrl: 'views/about.html',
+		controller: 'AboutController'
+	}).when('/quizzes/:quizId', {
+		templateUrl: 'views/quiz.html',
+		controller: 'QuizController'
+	}).otherwise({
+		redirectTo: '/quizzes'
+	});
+}]);
+
+quizd.controller('AboutController', ['$scope', function($scope){
+	
+}]);
