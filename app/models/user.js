dee4122ccd34fc0480e466e191211cd2efe0a410
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 12:14:10 +1100

    fixed up password hashing callback

--- app/models/user.js
+++ app/models/user.js
@@ -20,21 +20,15 @@ var UserSchema = new Schema({
 
 
 UserSchema.pre('save', function(next) {
-    var user = this;
+    if (!this.isModified('password')) return next();
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
-
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    bcrypt.genSalt(10, function(err, salt) {
         if (err) return next(err);
 
-        // hash the password along with our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
+        bcrypt.hash(this.password, salt, function(err, hash) {
             if (err) return next(err);
 
-            // override the cleartext password with the hashed one
-            user.password = hash;
+            this.password = hash;
             next();
         });
     });
