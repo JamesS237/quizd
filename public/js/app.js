4f04c226ff988430d6a0e5c722b9a642d855b906
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 13:57:34 +1100

    reformatted app.js and added some controller skeletons

--- public/js/app.js
+++ public/js/app.js
@@ -3,21 +3,33 @@
 var quizd = angular.module('quizd', ['ngRoute', 'ngResource']);
 
 quizd.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
 	$locationProvider.html5Mode(true);
+
 	$routeProvider.when('/about', {
+
 		templateUrl: 'views/about.html',
 		controller: 'StaticController'
+
 	}).when('/quizzes/:quizId', {
+
 		templateUrl: 'views/quiz.html',
 		controller: 'QuizController'
+
 	}).when('/', {
+
 		templateUrl: 'views/home.html',
 		controller: 'StaticController'
-	}).otherwise({
-		redirectTo: '/quizzes'
-	});
-}]);
 
-quizd.controller('StaticController', ['$scope', function($scope){
+	}).otherwise({
+		
+		redirectTo: '/'
+	});
+
+}]).controller('StaticController', ['$scope', function($scope){
+	
+}]).controller('QuizController', ['$scope', function($scope){
+	
+}]).controller('QuizListController', ['$scope', function($scope){
 	
 }]);
