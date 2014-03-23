4ef74d573400935482ccfce4523d068e83b30ae3
Arvoreniad <alexnielsen12@gmail.com>
2014-03-23 17:28:15 +1100

    set passport route config

--- app/routes.js
+++ app/routes.js
@@ -3,13 +3,10 @@
 module.exports = function(app, passport) {
 	var staticPages = require('./controllers/static');
 	var users = require('./controllers/users');
-	
+
 	app.post('/signup', users.create);
 
-	app.post('/authenticate', passport.authenticate('local', {
-        failureRedirect: '/signin',
-        failureFlash: true
-    }), users.authenticate);
+	app.post('/authenticate', passport.authenticate('local', {session: false}), users.authenticate);
 
 	app.get('/:anything', staticPages.home);
 	app.get('/', staticPages.home);
