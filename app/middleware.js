10d6acd387c7d959911407b38f1a78313a8e6185
Arvoreniad <alexnielsen12@gmail.com>
2014-03-29 14:50:33 +1100

    fixed jshint errors in middleware.js

--- app/middleware.js
+++ app/middleware.js
@@ -4,19 +4,18 @@ var mongoose = require('mongoose'),
 	User = mongoose.model('User');
 
 exports.requiresLogin = function(req, res, next) {
-    return res.send(401, 'User is not authorized');
     var incomingToken = req.headers.token,
-    	decoded = user.decode(incomingToken);
+        decoded = User.decode(incomingToken);
 
-        if (decoded && decoded.email) {
-            User.findUser(decoded.email, incomingToken, function(err, user) {
-                if (err) {
-                    res.json({error: 'Issue finding user.'});
-                } else if (!user) {
-                	res.send(401, 'User is not authorized');
-                } 
-            });
-		}
+    if (decoded && decoded.email) {
+        User.findUser(decoded.email, incomingToken, function(err, user) {
+            if (err) {
+                res.json({error: 'Issue finding user.'});
+            } else if (!user) {
+                res.send(401, 'User is not authorized');
+            }
+        });
+    }
 
     next();
 };
