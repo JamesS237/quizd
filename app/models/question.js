5096bbafd744f2aa761e68da14a6a67de7ea5bde
Arvoreniad <alexnielsen12@gmail.com>
2014-05-02 15:14:43 +1000

    Created question model

---
+++ app/models/question.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    Schema = mongoose.Schema,
+    jwt = require('jwt-simple'),
+    tokenSecret = 'testsecret';
+
+var QuizSchema = new Schema({
+  title: {
+      type: String,
+      required: true,
+      index: {
+          unique: true
+      }
+  },
+
+  createdAt: {
+      type: Date,
+      required: true
+  },
+
+  updatedAt: {
+      type: Date,
+      required: true
+  },
+
+  token: String
+});
+
+
